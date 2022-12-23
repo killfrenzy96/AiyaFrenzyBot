55a35dafca21e1b30507469d7f63e19ebb3f5851
killfrenzy96 <evan298@gmail.com>
2022-12-23 15:57:38 +1100

    updated message for fallback view handler

--- core/fallbackviewcog.py
+++ core/fallbackviewcog.py
@@ -35,7 +35,7 @@ class FallbackViewCog(commands.Cog, description='Create images from natural lang
             except:
                 # create a new view
                 stable_cog = self.bot.get_cog('StableCog')
-                input_object = await viewhandler.get_input_object(stable_cog, interaction)
+                input_object = await viewhandler.get_input_object(stable_cog, interaction, ' ')
                 view = viewhandler.DrawExtendedView(input_object)
                 viewhandler.fallback_views[message.id] = view
                 await view.button_extra_callback(interaction)
